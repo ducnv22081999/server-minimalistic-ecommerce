24502cd56bbdc6d1b61a1fb97e3347293a73ee25
DucNV <s2ducdaus2@gmail.com>
2022-01-04 16:54:37 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ const productRouter = require("./product");
 const categoryRouter = require("./category");
 
 function route(app) {
-  app.use("/product", productRouter);
-  app.use("/category", categoryRouter);
+  app.use("/api/product", productRouter);
+  app.use("/api/category", categoryRouter);
 }
 
 module.exports = route;
